ddbfacada14aa9c7eda59acb864d62f3fbf8fb37
Davidram1802 <56789396+Davidram1802@users.noreply.github.com>
2020-11-20 16:35:54 +0100

    añadido redis

--- main.py
+++ main.py
@@ -3,38 +3,27 @@ from fastapi import FastAPI,HTTPException
 from fastapi.responses import RedirectResponse
 from hash_function import create_short_link
 from datetime import datetime, timezone
-
-# class Item(BaseModel):
-#    name:str
-#    price:float
-
+import redis
 
 app = FastAPI()
 
-urls=[
-    {"url":"https://www.instagram.com", "url_shorted": 'insta'},
-    {"url":"https://www.youtube.com", "url_shorted":'vids'},
-    {"url":"https://www.google.com", "url_shorted":'buscar'}
-]
+re = redis.Redis(host='127.0.0.1',port= 6379 ) 
 
-'''
-@app.get("/")
-async def test(param1,param2):
-    return {"parametro1":param1,"parametro2":param2}'''
 
 @app.get("/")
 async def get_all():
-    return urls
+    keys_list=re.keys()
+    keys_values=[{'key':key,'value':re.get(key)} for key in keys_list]
+    return keys_values
 
 @app.get('/{url_shorted}')
 async def redirect(url_shorted:str):
-    url_to_redirect = None
-    for url in urls:
-        if url["url_shorted"] == url_shorted:
-            url_to_redirect=url["url"]
+    url_to_redirect=re.get(url_shorted)
+    print((url_to_redirect.decode('utf-8')))
+    
     if url_to_redirect is not None:
         # return {'url_to_redirect':url_to_redirect}
-        return RedirectResponse(url=url_to_redirect)
+        return  RedirectResponse(url= url_to_redirect.decode('utf-8') ,status_code=302)
     else:
         raise HTTPException(status_code=404,detail='The link does not exist, could not redirect.')
 
@@ -42,6 +31,9 @@ async def redirect(url_shorted:str):
 @app.post("/addurl/{url}")
 async def func(url:str):
     timestamp = datetime.now().replace(tzinfo=timezone.utc).timestamp()
-    new_url={"url":'http://' + url, "url_shorted":create_short_link(url,timestamp)}
-    urls.append(new_url)
-    return new_url # hashear la url (cambiar url2 por la funcion de hasheo)
+
+    new_url={"url": url, "url_shorted":create_short_link(url,timestamp)} # añadir http://
+
+    re.set(new_url['url_shorted'], new_url['url'])
+
+    return new_url 
