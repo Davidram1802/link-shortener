5f3f7056f7e3bb6fbc2ada1608a5b94ae2b1ce67
Davidram1802 <david@DESKTOP-S1D2BG1.localdomain>
2020-11-30 16:08:48 +0100

    inicio OAuth2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI,HTTPException,Depends
 #from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.responses import RedirectResponse
 from hash_function import create_short_link
 from datetime import datetime, timezone
@@ -9,6 +10,7 @@ app = FastAPI()
 
 re = redis.Redis(host='127.0.0.1',port= 6379 ) 
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @app.get("/")
 async def get_all():
@@ -16,10 +18,13 @@ async def get_all():
     keys_values=[{'key':key,'value':re.get(key)} for key in keys_list]
     return keys_values
 
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token} 
+
 @app.get('/{url_shorted}')
 async def redirect(url_shorted:str):
     url_to_redirect=re.get(url_shorted)
-    print((url_to_redirect.decode('utf-8')))
     
     if url_to_redirect is not None:
         # return {'url_to_redirect':url_to_redirect}
@@ -32,7 +37,7 @@ async def redirect(url_shorted:str):
 async def func(url:str):
     timestamp = datetime.now().replace(tzinfo=timezone.utc).timestamp()
 
-    new_url={"url": url, "url_shorted":create_short_link(url,timestamp)} # añadir http://
+    new_url={"url":' http://' + url, "url_shorted":create_short_link(url,timestamp)} # añadir http://
 
     re.set(new_url['url_shorted'], new_url['url'])
 
