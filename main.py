704ce73c60cb21d3520875e2bc2db6a758176ec5
Davidram1802 <david@DESKTOP-S1D2BG1.localdomain>
2020-12-05 23:57:38 +0100

    siguiendo OAuth

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI,HTTPException,Depends
-#from pydantic import BaseModel
+sifrom pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer
 from fastapi.responses import RedirectResponse
 from hash_function import create_short_link
 from datetime import datetime, timezone
 import redis
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
 
 app = FastAPI()
 
@@ -12,17 +18,35 @@ re = redis.Redis(host='127.0.0.1',port= 6379 )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
+
+
+@app.get("/items")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token} 
+
+
 @app.get("/")
 async def get_all():
     keys_list=re.keys()
     keys_values=[{'key':key,'value':re.get(key)} for key in keys_list]
     return keys_values
 
-@app.get("/items/")
-async def read_items(token: str = Depends(oauth2_scheme)):
-    return {"token": token} 
 
-@app.get('/{url_shorted}')
+@app.get('/{url_sorted}')
 async def redirect(url_shorted:str):
     url_to_redirect=re.get(url_shorted)
     
